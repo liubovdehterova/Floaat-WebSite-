f8f83c1c69c137f7693ef48f62f8e4be9805ece5
liubovdehterova <lubovdegtereva60@gmail.com>
2022-11-25 14:43:37 +0200

    Add scrollmagic, start 2 blocks

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const cursorPointed = document.querySelector('.pointed');
 const cursorRounded = document.querySelector('.rounded');
-const headerLogo = document.querySelector('.header__top__logo');
-const headerTopContactsLink = document.querySelector('.header__top__contacts__link');
-const headerTopContactsLinkDot = document.querySelector('.header__top__contacts__dot');
+const headerLogo = document.querySelectorAll('.header__top__logo');
+const headerTopContactsLink = document.querySelectorAll('.header__top__contacts__link');
+const headerTopContactsLinkDot = document.querySelectorAll('.header__top__contacts__dot');
 const headerMainText = document.querySelector('.header__main__text__animation');
+const clientsCard = document.querySelectorAll('.clients__info__main__card');
 
 const moveCursor = (e) => {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
+    const mouseY = e.pageY;
+    const mouseX = e.pageX;
     cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
     cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
-window.addEventListener('mousemove', moveCursor);
+document.body.addEventListener('mousemove', moveCursor);
 
 function addClassPointerLogo(e) {
     if (e.type === 'mouseover') {
@@ -22,8 +23,11 @@ function addClassPointerLogo(e) {
         cursorRounded.classList.remove('rounded__logo');
     }
 }
-
 headerLogo.onmouseover = headerLogo.onmouseout = addClassPointerLogo;
+
+clientsCard.forEach((e) => {
+    e.onmouseover = e.onmouseout = addClassPointerLogo;
+});
 
 function addClassPointerContacts(e) {
     if (e.type === 'mouseover') {
@@ -34,20 +38,24 @@ function addClassPointerContacts(e) {
         cursorRounded.classList.remove('rounded__contacts');
     }
 }
-
-headerTopContactsLink.onmouseover = headerTopContactsLink.onmouseout = addClassPointerContacts;
+headerTopContactsLink.forEach((e) => {
+    e.onmouseover = e.onmouseout = addClassPointerContacts;
+});
 
 function addClassPointerDot(e) {
     if (e.type === 'mouseover') {
         cursorPointed.classList.add('pointer__dot');
         cursorRounded.classList.add('rounded__contacts');
+
     } else if (e.type === 'mouseout') {
         cursorPointed.classList.remove('pointer__dot');
         cursorRounded.classList.remove('rounded__contacts');
     }
 }
+headerTopContactsLinkDot.forEach((e) => {
+    e.onmouseover = e.onmouseout = addClassPointerDot;
+});
 
-headerTopContactsLinkDot.onmouseover = headerTopContactsLinkDot.onmouseout = addClassPointerDot;
 
 function headerMainTextAnimation(e) {
     let mouseX = e.clientX;
@@ -61,3 +69,24 @@ function headerMainTextAnimation(e) {
 
 window.addEventListener("mousemove", headerMainTextAnimation);
 
+(function () {
+    let controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 0
+        }
+    });
+
+
+    let panels = document.querySelectorAll('.panel');
+
+    for (let panel of panels) {
+
+        let scene = new ScrollMagic.Scene({
+            triggerElement: panel
+        })
+            .addIndicators()
+            .on('progress')
+            .setPin(panel)
+            .addTo(controller);
+    }
+})();
